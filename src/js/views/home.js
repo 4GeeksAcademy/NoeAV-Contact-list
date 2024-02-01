c7110a45ed29ba25def689146b9998498958c61b
Noe CM <128952542+NoeAV1314@users.noreply.github.com>
2024-02-01 07:17:56 +0000

    Base

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,102 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+		<div className="text-center auto">
+			<ul>
+
+			<div className="card mb-3">
+  				<div className="row g-0">
+    				<div className="col-md-3">
+      					<p className="rounded float-start"> Picture </p>
+    				</div>
+					<div className="col-md-5">
+						<h5 className="card-title">Card title</h5>
+					<ul>
+						<div> Extra info </div>
+						<div> Extra info </div>
+						<div> Extra info </div>
+					</ul>
+					</div>	
+					<div className="col">
+						<i class="fa-solid fa-user-pen" style={{color: "#08a305"}}></i>
+						<i className="fa-solid fa-trash-can" style={{color: "#08a305"}} 
+						onClick={() => 
+							eliminarTarea (index)	
+						}></i>
+					</div>	
+				</div>
+				</div>
+
+				<div className="card mb-3">
+  				<div className="row g-0">
+    				<div className="col-md-3">
+      					<p className="rounded float-start"> Picture two</p>
+    				</div>
+					<div className="col-md-5">
+						<h5 className="card-title">Card title</h5>
+					<ul>
+						<div> Extra info </div>
+						<div> Extra info </div>
+						<div> Extra info </div>
+					</ul>
+					</div>	
+					<div className="col">
+						<i class="fa-solid fa-user-pen" style={{color: "#08a305"}}></i>
+						<i className="fa-solid fa-trash-can" style={{color: "#08a305"}} 
+						onClick={() => 
+							eliminarTarea (index)	
+						}></i>
+					</div>	
+				</div>
+				</div>
+
+				<div className="card mb-3">
+  				<div className="row g-0">
+    				<div className="col-md-3">
+      					<p className="rounded float-start"> Picture three</p>
+    				</div>
+					<div className="col-md-5">
+						<h5 className="card-title">Card title</h5>
+					<ul>
+						<div> Extra info </div>
+						<div> Extra info </div>
+						<div> Extra info </div>
+					</ul>
+					</div>	
+					<div className="col">
+						<i class="fa-solid fa-user-pen" style={{color: "#08a305"}}></i>
+						<i className="fa-solid fa-trash-can" style={{color: "#08a305"}} 
+						onClick={() => 
+							eliminarTarea (index)	
+						}></i>
+					</div>	
+				</div>
+				</div>
+
+				<div className="card mb-3">
+  				<div className="row g-0">
+    				<div className="col-md-3">
+      					<p className="rounded float-start"> Picture four </p>
+    				</div>
+					<div className="col-md-5">
+						<h5 className="card-title">Card title</h5>
+					<ul>
+						<div> Extra info </div>
+						<div> Extra info </div>
+						<div> Extra info </div>
+					</ul>
+					</div>	
+					<div className="col">
+						<i class="fa-solid fa-user-pen" style={{color: "#08a305"}}></i>
+						<i className="fa-solid fa-trash-can" style={{color: "#08a305"}} 
+						onClick={() => 
+							eliminarTarea (index)	
+						}></i>
+					</div>	
+				</div>
+				</div>
+			
+			</ul>
+		</div>
 );
