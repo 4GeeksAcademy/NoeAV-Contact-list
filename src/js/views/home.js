d9a09ec54bd62bd8ba3d5a9f390138b40cdb3e46
Noe CM <128952542+NoeAV1314@users.noreply.github.com>
2024-02-04 08:02:00 +0000

    undone

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "../../styles/home.css";
 
 export const Home = () => {
-	// const [contact, setContact] = useState(0);
+	const [inputValue, setInputValue] = useState("")
+	const [contact, setContact] = useState(0);
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setInputValue("");
+        setContact([...contact, {label: inputValue, done: false}]);
+            updateContact([...contact, {label: inputValue, done: false}]);
+    }
+
+    // 1. getList - para traer la lista de contactos actual
+    // 2. updateContact - para añadir contacto a la base y editar la información del contacto
+    // 3. deleteContact - para borrar contacto ya creado.
+
+    function getList() {
+        fetch(url)
+        .then((resp) => resp.json())
+        .then((data) => setContact(data))
+    };
+
+    // 2. addContact - para añadir contacto a la base
+    function updateContact(updatedContact){
+		fetch("", {
+            method: "PUT",
+            body: JSON.stringify(updatedContact),
+            headers: {
+                "Content-Type": "application/json",
+            },
+            })
+            .then((resp) => {
+                return resp.json();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+	  };
+
+    // 3. deleteContact - para que pueda editar la información del contacto
+
+    const deleteContact = (label) => {
+        const updatedContact = contact.filter((item) => item.label !== label);
+        updateContact(updatedContact);
+        setContact(updatedContact);
+    }
+    
     return (
     <div className="text-center auto">
         <ul>
@@ -15,90 +58,23 @@ export const Home = () => {
                 </div>
                 <div className="col-md-5">
                     <h5 className="card-title">Card title</h5>
-                <ul>
-                    <div> Extra info </div>
-                    <div> Extra info </div>
-                    <div> Extra info </div>
+                <ul className="extrainfo">
+                    <i class="fa-solid fa-phone"></i><div> Extra info </div>
+                    <i class="fa-solid fa-location-dot"></i><div> Extra info </div>
+                    <i class="fa-solid fa-envelope-open-text"></i><div> Extra info </div>
                 </ul>
                 </div>	
                 <div className="col">
-                    <i class="fa-solid fa-user-pen" style={{color: "#08a305"}}></i>
-                    <i className="fa-solid fa-trash-can" style={{color: "#08a305"}} 
+                    <i className="fa-solid fa-user-pen" style={{color: "1f3151"}} 
                     onClick={() => 
-                        eliminarTarea (index)	
-                    }></i>
+                        editContact (index)}></i>
+                    <i className="fa-solid fa-trash-can" style={{color: "1f3151"}} 
+                    onClick={() => 
+                        deleteContact (index)}></i>
                 </div>	
             </div>
             </div>
 
-            <div className="card mb-3">
-              <div className="row g-0">
-                <div className="col-md-3">
-                      <p className="rounded float-start"> </p>
-                </div>
-                <div className="col-md-5">
-                    <h5 className="card-title">Card title</h5>
-                <ul>
-                    <div> Extra info </div>
-                    <div> Extra info </div>
-                    <div> Extra info </div>
-                </ul>
-                </div>	
-                <div className="col">
-                    <i class="fa-solid fa-user-pen" style={{color: "#08a305"}}></i>
-                    <i className="fa-solid fa-trash-can" style={{color: "#08a305"}} 
-                    onClick={() => 
-                        eliminarTarea (index)	
-                    }></i>
-                </div>	
-            </div>
-            </div>
-
-            <div className="card mb-3">
-              <div className="row g-0">
-                <div className="col-md-3">
-                      <p className="rounded float-start"> Picture three</p>
-                </div>
-                <div className="col-md-5">
-                    <h5 className="card-title">Card title</h5>
-                <ul>
-                    <div> Extra info </div>
-                    <div> Extra info </div>
-                    <div> Extra info </div>
-                </ul>
-                </div>	
-                <div className="col">
-                    <i class="fa-solid fa-user-pen" style={{color: "#08a305"}}></i>
-                    <i className="fa-solid fa-trash-can" style={{color: "#08a305"}} 
-                    onClick={() => 
-                        eliminarTarea (index)	
-                    }></i>
-                </div>	
-            </div>
-            </div>
-
-            <div className="card mb-3">
-              <div className="row g-0">
-                <div className="col-md-3">
-                      <p className="rounded float-start"> Picture four </p>
-                </div>
-                <div className="col-md-5">
-                    <h5 className="card-title">Card title</h5>
-                <ul>
-                    <div> Extra info </div>
-                    <div> Extra info </div>
-                    <div> Extra info </div>
-                </ul>
-                </div>	
-                <div className="col">
-                    <i class="fa-solid fa-user-pen" style={{color: "#08a305"}}></i>
-                    <i className="fa-solid fa-trash-can" style={{color: "#08a305"}} 
-                    onClick={() => 
-                        eliminarTarea (index)	
-                    }></i>
-                </div>	
-            </div>
-            </div>
         
         </ul>
     </div>
